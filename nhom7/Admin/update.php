fc247f47aafeac7f910949440ecaa67fe3b3e801
truong kien <truongkien0815@gmail.com>
2021-12-20 20:12:53 +0700

    kien

--- nhom7/Admin/update.php
+++ nhom7/Admin/update.php
@@ -15,12 +15,12 @@ require "models/manufactures.php";
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>Project Edit</h1>
+            <h1>Product Edit</h1>
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Project Edit</li>
+              <li class="breadcrumb-item active">Product Edit</li>
             </ol>
           </div>
         </div>
